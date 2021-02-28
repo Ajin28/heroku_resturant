ed6dd91ff215afb294ca621003cda67055e6af8d
Ajin28 <amisha28jain@gmail.com>
2021-02-28 16:15:58 +0530

    Heroku push 1

--- client/src/shared/baseUrl.js
+++ client/src/shared/baseUrl.js
@@ -1,1 +1,4 @@
-export const baseUrl = 'http://localhost:5000/';
+console.log("BASE URL", process.env.BASE_URL);
+export const baseUrl = process.env.BASE_URL || 'https://react-restuarant-confusion.herokuapp.com/';
+
+//dev http://localhost:5000/
--- client/src/shared/mapApi.js
+++ client/src/shared/mapApi.js
@@ -1,1 +1,2 @@
-export const mapApiKey = "AIzaSyD91cemAO0vZ4TKcbPBlLOkjMYAjy76n1o&q=place_id:ChIJi6HSmsSvgTkRsZ4zmAbstXk"
+console.log("MAP API", process.env.MAP_API_KEY);
+export const mapApiKey = process.env.MAP_API_KEY || "AIzaSyD91cemAO0vZ4TKcbPBlLOkjMYAjy76n1o&q=place_id:ChIJi6HSmsSvgTkRsZ4zmAbstXk"
